b5c5df0d4901885c86e8dea5ac66b6ffc5f97bdb
O <I aM O>
2024-08-24 08:47:28 -0400

    Improve save file read/write speed

--- palworld_save_pal/save_file/save_file.py
+++ palworld_save_pal/save_file/save_file.py
@@ -6,7 +6,11 @@ from uuid import UUID
 import uuid
 from pydantic import BaseModel, ConfigDict, PrivateAttr
 
-from palworld_save_tools.archive import UUID as ArchiveUUID
+from palworld_save_tools.archive import (
+    UUID as ArchiveUUID,
+    FArchiveReader,
+    FArchiveWriter,
+)
 from palworld_save_tools.gvas import GvasFile
 from palworld_save_tools.json_tools import CustomEncoder
 from palworld_save_tools.palsav import compress_gvas_to_sav, decompress_sav_to_gvas
@@ -33,9 +37,104 @@ from palworld_save_pal.save_file.empty_objects import get_empty_property, Proper
 
 logger = create_logger(__name__)
 
+
+def skip_decode(reader: FArchiveReader, type_name: str, size: int, path: str):
+    if type_name == "ArrayProperty":
+        array_type = reader.fstring()
+        value = {
+            "skip_type": type_name,
+            "array_type": array_type,
+            "id": reader.optional_guid(),
+            "value": reader.read(size),
+        }
+    elif type_name == "MapProperty":
+        key_type = reader.fstring()
+        value_type = reader.fstring()
+        _id = reader.optional_guid()
+        value = {
+            "skip_type": type_name,
+            "key_type": key_type,
+            "value_type": value_type,
+            "id": _id,
+            "value": reader.read(size),
+        }
+    elif type_name == "StructProperty":
+        value = {
+            "skip_type": type_name,
+            "struct_type": reader.fstring(),
+            "struct_id": reader.guid(),
+            "id": reader.optional_guid(),
+            "value": reader.read(size),
+        }
+    else:
+        raise Exception(
+            f"Expected ArrayProperty or MapProperty or StructProperty, got {type_name} in {path}"
+        )
+    return value
+
+
+def skip_encode(writer: FArchiveWriter, property_type: str, properties: dict) -> int:
+    if "skip_type" not in properties:
+        if properties["custom_type"] in PALWORLD_CUSTOM_PROPERTIES is not None:
+            return PALWORLD_CUSTOM_PROPERTIES[properties["custom_type"]][1](
+                writer, property_type, properties
+            )
+        else:
+            # Never be run to here
+            return writer.property_inner(writer, property_type, properties)
+    if property_type == "ArrayProperty":
+        del properties["custom_type"]
+        del properties["skip_type"]
+        writer.fstring(properties["array_type"])
+        writer.optional_guid(properties.get("id", None))
+        writer.write(properties["value"])
+        return len(properties["value"])
+    elif property_type == "MapProperty":
+        del properties["custom_type"]
+        del properties["skip_type"]
+        writer.fstring(properties["key_type"])
+        writer.fstring(properties["value_type"])
+        writer.optional_guid(properties.get("id", None))
+        writer.write(properties["value"])
+        return len(properties["value"])
+    elif property_type == "StructProperty":
+        del properties["custom_type"]
+        del properties["skip_type"]
+        writer.fstring(properties["struct_type"])
+        writer.guid(properties["struct_id"])
+        writer.optional_guid(properties.get("id", None))
+        writer.write(properties["value"])
+        return len(properties["value"])
+    else:
+        raise Exception(
+            f"Expected ArrayProperty or MapProperty or StructProperty, got {property_type}"
+        )
+
+
 CUSTOM_PROPERTIES = {
     k: v for k, v in PALWORLD_CUSTOM_PROPERTIES.items() if k not in DISABLED_PROPERTIES
 }
+CUSTOM_PROPERTIES[".worldSaveData.MapObjectSaveData"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.FoliageGridSaveDataMap"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.MapObjectSpawnerInStageSaveData"] = (
+    skip_decode,
+    skip_encode,
+)
+CUSTOM_PROPERTIES[".worldSaveData.WorkSaveData"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.DungeonSaveData"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.EnemyCampSaveData"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.CharacterParameterStorageSaveData"] = (
+    skip_decode,
+    skip_encode,
+)
+CUSTOM_PROPERTIES[".worldSaveData.InvaderSaveData"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.DungeonPointMarkerSaveData"] = (
+    skip_decode,
+    skip_encode,
+)
+CUSTOM_PROPERTIES[".worldSaveData.GameTimeSaveData"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.OilrigSaveData"] = (skip_decode, skip_encode)
+CUSTOM_PROPERTIES[".worldSaveData.SupplySaveData"] = (skip_decode, skip_encode)
 
 
 class SaveFile(BaseModel):
@@ -125,7 +224,7 @@ class SaveFile(BaseModel):
         else:
             save_type = 0x31
         gvas = copy.deepcopy(self._gvas_file)
-        return compress_gvas_to_sav(gvas.write(PALWORLD_CUSTOM_PROPERTIES), save_type)
+        return compress_gvas_to_sav(gvas.write(CUSTOM_PROPERTIES), save_type)
 
     def to_json_file(
         self,
@@ -158,7 +257,7 @@ class SaveFile(BaseModel):
         logger.info("Compressing GVAS to SAV with save type %s", save_type)
 
         sav_file = compress_gvas_to_sav(
-            self._gvas_file.write(PALWORLD_CUSTOM_PROPERTIES), save_type
+            self._gvas_file.write(CUSTOM_PROPERTIES), save_type
         )
         with open(output_path, "wb") as f:
             f.write(sav_file)
