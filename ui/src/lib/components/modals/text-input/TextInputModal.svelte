d59bc0c89ebe82dbe793a840a43cdf6efa027415
I_aM_O <orod41285@gmail.com>
2024-12-08 08:31:16 -0500

    refactor: change text input modal layout

--- ui/src/lib/components/modals/text-input/TextInputModal.svelte
+++ ui/src/lib/components/modals/text-input/TextInputModal.svelte
@@ -6,10 +6,12 @@
 	let {
 		title = '',
 		value = $bindable(''),
+		inputLabel,
 		closeModal
 	} = $props<{
 		title?: string;
 		value?: string;
+		inputLabel?: string;
 		closeModal: (value: any) => void;
 	}>();
 
@@ -21,27 +23,29 @@
 <Card class="bg-surface-500 min-w-[calc(100vw/3)]">
 	<h3 class="h3">{title}</h3>
 
-	<div class="mt-2 flex flex-row items-center space-x-2">
-		<Input inputClass="grow" bind:value />
-		<Tooltip position="bottom">
-			{#snippet children()}
-				<button class="btn hover:bg-secondary-500 px-2" onclick={() => handleClose(value)}>
-					<Save />
-				</button>
-			{/snippet}
-			{#snippet popup()}
-				<span>Save</span>
-			{/snippet}
-		</Tooltip>
-		<Tooltip position="bottom">
-			{#snippet children()}
-				<button class="btn hover:bg-secondary-500 px-2" onclick={() => handleClose(null)}>
-					<X />
-				</button>
-			{/snippet}
-			{#snippet popup()}
-				<span>Cancel</span>
-			{/snippet}
-		</Tooltip>
+	<div class="mt-2 flex flex-col space-x-2">
+		<Input inputClass="grow" bind:value label={inputLabel} />
+		<div class="mt-2 flex justify-end">
+			<Tooltip position="bottom">
+				{#snippet children()}
+					<button class="btn hover:bg-secondary-500 px-2" onclick={() => handleClose(value)}>
+						<Save />
+					</button>
+				{/snippet}
+				{#snippet popup()}
+					<span>Save</span>
+				{/snippet}
+			</Tooltip>
+			<Tooltip position="bottom">
+				{#snippet children()}
+					<button class="btn hover:bg-secondary-500 px-2" onclick={() => handleClose(null)}>
+						<X />
+					</button>
+				{/snippet}
+				{#snippet popup()}
+					<span>Cancel</span>
+				{/snippet}
+			</Tooltip>
+		</div>
 	</div>
 </Card>
