7c8aa4fa40d53576e7c5fe2ab0a6023c61a96dda
I_aM_O <orod41285@gmail.com>
2024-12-08 08:30:20 -0500

    feat: active and passive skills presets

--- ui/src/lib/components/ui/section-header/SectionHeader.svelte
+++ ui/src/lib/components/ui/section-header/SectionHeader.svelte
@@ -9,7 +9,7 @@
 
 <div class="relative inline-block w-full">
 	<span class="relative px-2 text-lg font-bold">{text}</span>
-	<div class="bg-surface-800/50 absolute inset-x-0 bottom-0 h-1/2 opacity-5 dark:opacity-50"></div>
+	<div class="bg-surface-900 absolute inset-x-0 bottom-0 -z-50 h-1/2"></div>
 	{#if action}
 		<div class="absolute right-0 top-1/2 z-50 -translate-y-1/2">
 			{@render action()}
