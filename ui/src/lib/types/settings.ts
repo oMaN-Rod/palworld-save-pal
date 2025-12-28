982514f79bfa330c71c3aa1cc3c1dce4e1590404
I_aM_O <orod41285@gmail.com>
2025-12-28 10:50:33 -0500

    Add localizations for - es-MX: (Spanish - Mexico) - id: (Indonesian) - pl: (Polish) - th: (Thai) - tr: (Turkish) - vi: (Vietnamese)

--- ui/src/lib/types/settings.ts
+++ ui/src/lib/types/settings.ts
@@ -2,11 +2,17 @@ export type SupportedLanguage =
 	| 'de'
 	| 'en'
 	| 'es'
+	| 'es-MX'
 	| 'fr'
+	| 'id'
 	| 'it'
 	| 'ko'
+	| 'pl'
 	| 'pt-BR'
 	| 'ru'
+	| 'th'
+	| 'tr'
+	| 'vi'
 	| 'zh-Hans'
 	| 'zh-Hant';
 
@@ -14,11 +20,17 @@ export const languages: Record<SupportedLanguage, string> = {
 	de: 'Deutsch',
 	en: 'English',
 	es: 'Español',
+	'es-MX': 'Español (México)',
 	fr: 'Français',
+	id: 'Bahasa Indonesia',
 	it: 'Italiano',
 	ko: '한국어',
+	pl: 'Polski',
 	'pt-BR': 'Português',
 	ru: 'Русский',
+	th: 'ไทย',
+	tr: 'Türkçe',
+	vi: 'Tiếng Việt',
 	'zh-Hans': '简体中文',
 	'zh-Hant': '繁體中文'
 };
