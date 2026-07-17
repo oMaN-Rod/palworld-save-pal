dc9a593170945abea5c93bd71cbabeaf252299e1
I_aM_O <orod41285@gmail.com>
2026-07-17 13:32:48 -0400

    feat: add WorldOption editor modal

---
+++ ui/src/lib/components/worldoption/index.ts
@@ -0,0 +1,5 @@
+export { default as WorldOptionModal } from './WorldOptionModal.svelte';
+export { default as WorldOptionField } from './WorldOptionField.svelte';
+export { default as CrossplayPlatformsField } from './CrossplayPlatformsField.svelte';
+export { default as DenyTechnologyField } from './DenyTechnologyField.svelte';
+export * from './worldOptionFields';
